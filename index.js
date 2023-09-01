19c420a56419d9b514b0fbe665d53412aa5cdd2a
lebrancconvas <lebrancconvas@gmail.com>
2023-09-01 18:57:04 +0700

    ✨ feat(merge): update route

--- index.js
+++ index.js
@@ -10,6 +10,11 @@ app.get('/dev', (req, res) => {
   res.send('Hello Dev');
 });
 
+
+app.get('/merge', (req, res) => {
+  res.send('Hello Merge');
+});
+
 app.listen(8008, () => {
   console.log('Server is running on port 8008');
 });
