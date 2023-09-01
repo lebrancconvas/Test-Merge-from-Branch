ee635d3e4a7c568f63daea46cad52e81276ee053
lebrancconvas <lebrancconvas@gmail.com>
2023-09-01 18:52:04 +0700

    ✨ feat(dev): update route

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('Hello Main');
 });
 
+app.get('/dev', (req, res) => {
+  res.send('Hello Dev');
+});
+
 app.listen(8008, () => {
   console.log('Server is running on port 8008');
 });
