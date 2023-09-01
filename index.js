1cc540c153de36ad06c580492da60074e2bba7f2
lebrancconvas <lebrancconvas@gmail.com>
2023-09-01 18:50:03 +0700

    ✨ feat: complete server

--- index.js
+++ index.js
@@ -3,5 +3,9 @@ const app = express();
 
 
 app.get('/', (req, res) => {
-  res.send('Hello Main'); 
+  res.send('Hello Main');
+});
+
+app.listen(8008, () => {
+  console.log('Server is running on port 8008');
 });
